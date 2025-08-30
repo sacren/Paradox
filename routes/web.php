478ad34ad854c6b561125daa6bc0824f8c11f0d4
Jean Sacren <sakiwit@gmail.com>
2025-08-30 16:59:36 -0600

    Fix GET method not supported for route logout
    GET /logout shows this error:
    
    'The GET method is not supported for route logout. Supported methods: POST.'
    
    By design the logout route supports POST only.  Fix this error by fully
    disabling GET /logout.

--- routes/web.php
+++ routes/web.php
@@ -11,5 +11,9 @@ Route::get('dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::get('logout', function () {
+    abort(404);
+});
+
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
