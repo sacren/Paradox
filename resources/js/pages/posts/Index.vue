2c2ff3f7e63ca03b37b2ec5bf4de035b82f47a3d
Jean Sacren <sakiwit@gmail.com>
2025-09-05 17:42:09 -0600

    Build date formatting helper
    Add date formatting helper and use it for proper date formatting.

--- resources/js/pages/posts/Index.vue
+++ resources/js/pages/posts/Index.vue
@@ -31,6 +31,17 @@
             required: true,
         },
     });
+
+    function formatDate(dateString: string) {
+        return new Intl.DateTimeFormat('en-US', {
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric',
+            hour: '2-digit',
+            minute: '2-digit',
+            second: '2-digit',
+        }).format(new Date(dateString));
+    }
 </script>
 
 <template>
@@ -44,7 +55,7 @@
                     <div class="mt-1 text-blue-100">
                         By <span class="font-semibold">{{ post.user.name }}</span>
                         on <time :datetime="post.created_at">
-                            {{ new Date(post.created_at).toLocaleString() }}
+                            {{ formatDate(post.created_at) }}
                         </time>
                     </div>
                 </header>
