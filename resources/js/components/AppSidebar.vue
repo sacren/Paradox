4ab3c9ef7859ac95472aa06fcf212926a9b5a7b3
Jean Sacren <sakiwit@gmail.com>
2025-09-16 21:41:41 -0600

    Update sidebar links
    Use the Github Repo of this app to replace the vue-starter-kit.  Remove
    the Documentation link as it is not applicable.

--- resources/js/components/AppSidebar.vue
+++ resources/js/components/AppSidebar.vue
@@ -24,13 +24,8 @@ const mainNavItems: NavItem[] = [
 const footerNavItems: NavItem[] = [
     {
         title: 'Github Repo',
-        href: 'https://github.com/laravel/vue-starter-kit',
+        href: 'https://github.com/sacren/Paradox',
         icon: Folder,
-    },
-    {
-        title: 'Documentation',
-        href: 'https://laravel.com/docs/starter-kits#vue',
-        icon: BookOpen,
     },
 ];
 </script>
