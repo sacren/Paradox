4bb6687ee447df8d28ef83d59b6ac6807dbf5fa3
Jean Sacren <sakiwit@gmail.com>
2025-09-26 21:48:05 -0600

    Clear errors when add input
    By default, Inertia keeps validation errors until the next submission.
    Add auto-clear of all errors as the user enters the input.

--- resources/js/pages/posts/Index.vue
+++ resources/js/pages/posts/Index.vue
@@ -113,6 +113,7 @@
                         <textarea
                             id="content"
                             v-model="form.content"
+                            @input="form.clearErrors()"
                             rows="3"
                             class="w-full px-4 py-2 border border-gray-300 rounded-md
                                     focus:ring-blue-500 focus:border-blue-500"
