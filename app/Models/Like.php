5d56255bd848e78d1f6968b76fd70facf5425cb1
Jean Sacren <sakiwit@gmail.com>
2025-10-09 17:55:49 -0600

    Build Like model for mass assignment and relationship
    Use $fillable to enables safe mass assignment for web requests while
    keeping the model secure.  Since Like model only has two meaningful
    fields (and no sensitive ones), this is perfect.  For timestamps like
    created_at, updated_at, we don't need to include them in $fillable as
    Eloquent handles them automatically.
    
    Build belogsTo relationship between Like and User and between Like and
    Post.  Eloquent automatically assumes the foreign key is {relation}_id,
    so user() → user_id, post() → post_id.  No need to specify while we use
    the default column name.

--- app/Models/Like.php
+++ app/Models/Like.php
@@ -4,9 +4,32 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Like extends Model
 {
     /** @use HasFactory<\Database\Factories\LikeFactory> */
     use HasFactory;
+
+    protected $fillable = ['user_id', 'post_id'];
+
+    /**
+     * Get the user that owns the Like
+     *
+     * @return BelongsTo
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    /**
+     * Get the post that owns the Like
+     *
+     * @return BelongsTo
+     */
+    public function post(): BelongsTo
+    {
+        return $this->belongsTo(Post::class);
+    }
 }
