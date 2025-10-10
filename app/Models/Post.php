a3587a4b5f430435c2ed72cbbeeab493c2b33ad2
Jean Sacren <sakiwit@gmail.com>
2025-10-10 15:48:05 -0600

    Build likes() and likedBy() in the Post model
    Build inverse relationships in Post model to complement those in User
    model.
    
    1) likes() → HasMany(Like::class)
    2) likedBy() → BelongsToMany(User::class, 'likes')
    
    The second correctly models the many-to-many relationship between posts
    and users via the likes pivot table.  The clear and standard naming of
    likedBy is a common and intuitive name for this inverse relationship.
    
    Now we can do these:
    
    $post->likedBy → users who liked it
    $post->likes → raw like records

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -5,6 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Post extends Model
 {
@@ -23,9 +25,31 @@ class Post extends Model
 
     /**
      * Get the user that owns the Post
+     *
+     * @return BelongsTo
      */
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
+    }
+
+    /**
+     * Get the likes for the Post
+     *
+     * @return HasMany
+     */
+    public function likes(): HasMany
+    {
+        return $this->hasMany(Like::class);
+    }
+
+    /**
+     * Get the users who liked the Post
+     *
+     * @return BelongsToMany
+     */
+    public function likedBy(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class, 'likes');
     }
 }
